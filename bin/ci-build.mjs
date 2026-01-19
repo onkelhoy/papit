65ae84d361998c52051daee4ab297bb3abf04ad0
Henry <henry.pap@consid.se>
2026-01-19 09:49:30 +0100

    wip: workflow and bin scritps

---
+++ bin/ci-build.mjs
@@ -0,0 +1,4 @@
+// import 
+
+// TODO: buld package with remote flag should build only changed packages, but also their ancestors, so two loops, one to find all packages and then 
+// again to build those (the second time it should not call with remote as it doesnt need to refetch)
--- bin/prebuild.mjs
+++ bin/prebuild.mjs
@@ -5,13 +5,14 @@ import { spawnCommand } from "./spawn-command.mjs";
 const root = process.cwd();
 
 if (fs.existsSync(path.join(root, "node_modules/.bin/papit-build")))
-  process.exit(0);
+    process.exit(0);
 
 (async function () {
-  await spawnCommand("npm run prebuild", path.join(root, "packages/runtime/cli/build"));
-  await spawnCommand("npm run prebuild", path.join(root, "packages/runtime/cli/util"));
-  await spawnCommand("npm run build -- --force", path.join(root, "packages/runtime/cli/build"));
+    await spawnCommand("npm run prebuild", path.join(root, "packages/runtime/cli/build"));
+    await spawnCommand("npm run prebuild", path.join(root, "packages/runtime/cli/util"));
+    await spawnCommand("npm run build -- --force", path.join(root, "packages/runtime/cli/build"));
+    await spawnCommand("npm run build", path.join(root, "packages/runtime/cli/server")); // making sure we can start things like test server for CI 
 
-  await spawnCommand("npm install", root);
+    await spawnCommand(process.env.CI ? "npm ci" : "npm install", root);
 }());
 
