0e079a367ffa67d305b364eb1792f2437406b1fd
Henry <henry.pap@consid.se>
2026-08-12 13:48:07 +0200

    add: line component

---
+++ packages/game/shape/tests/line/unit.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  // Navigate to your test page
+  await page.goto('tests/line/');
+});
+
+declare global {
+  interface Window {
+    EVENT_EMITTED: any;
+  }
+}
+
+test.describe.skip("@papit/game-shape unit tests", () => {
+  test('available in DOM', async ({ page }) => {
+    // honestly dont know what to test 
+  });
+});
