ff55d7fead33d7a3506434f443d9b64b31819ea6
Henry <henry.pap@consid.se>
2026-08-12 14:02:47 +0200

    add: rectangle component

---
+++ packages/game/intersection/tests/rectangle/unit.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  // Navigate to your test page
+  await page.goto('tests/rectangle/');
+});
+
+declare global {
+  interface Window {
+    EVENT_EMITTED: any;
+  }
+}
+
+test.describe.skip("@papit/intersection unit tests", () => {
+  test('available in DOM', async ({ page }) => {
+    // honestly dont know what to test 
+  });
+});
